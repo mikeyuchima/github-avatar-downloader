d9c960c8b0524a225b1f353654c70c20b5b927de
Michael Surya <tomichaels@gmail.com>
2018-07-09 23:42:03 +0000

    refactored

--- download_avatars.js
+++ download_avatars.js
@@ -40,11 +40,6 @@ getRepoContributors("jquery", "jquery", function(err, result) {
 
 function downloadImageByURL(url, filePath) {
     request.get(url)
-    .on('error', function (err) {
-      throw err; 
-    })
-    .on('response', function (response) {
-      console.log('Response Status Code: ', response.statusCode);
-    })
+
     .pipe(fs.createWriteStream('./' + filePath + '.jpg'));
     };
