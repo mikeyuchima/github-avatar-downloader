dd04146c47495b9765af89bded87168c6a33d551
Michael Surya <tomichaels@gmail.com>
2018-07-09 23:36:10 +0000

    added download to filePath functionality

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 var ignore = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
@@ -7,21 +8,43 @@ function getRepoContributors(repoOwner, repoName, cb) {
     var options = {
       url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
       headers: {
-        'User-Agent': 'request'
+        'User-Agent': 'request',
+        'Authorization' : ignore.GITHUB_TOKEN
       }
     };
   
     request(options, function(err, res, body) {
-      var info = JSON.parse(body);
-        cb(err, info);
-    });
-  }
+      if (err) {
+        cb(err, null)
+      }
+      else {
+        var info = JSON.parse(body);
+        cb(null, info);
+      }
+    })
+  };
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-    console.log("Errors:", err);
+    if (err) {
+        console.log("Errors:", err);
+        return
+    }
     // console.log("Result:", result);
     result.forEach(element => {
-        console.log(element.avatar_url); 
+        const url = element.avatar_url;
+        const filePath = element.login;
+        console.log(element.avatar_url);
+        downloadImageByURL(url, filePath)
       });
 });
 
+function downloadImageByURL(url, filePath) {
+    request.get(url)
+    .on('error', function (err) {
+      throw err; 
+    })
+    .on('response', function (response) {
+      console.log('Response Status Code: ', response.statusCode);
+    })
+    .pipe(fs.createWriteStream('./' + filePath + '.jpg'));
+    };
